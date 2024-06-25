0f5c713a1907a603c7b92ba6ae1d7b1fa94621ef
Tiago Caldas <tiago.rodrigues.caldas@gmail.com>
2024-06-25 00:48:31 +0000

    delete subnets and route tables

--- vpc/bash-scripts/delete-vpc.sh
+++ vpc/bash-scripts/delete-vpc.sh
@@ -26,6 +26,32 @@ for IGW_ID in $IGW_IDS; do
     aws ec2 delete-internet-gateway --internet-gateway-id $IGW_ID
 done
 
+# Delete all subnets in the VPC
+echo "== Deleting subnets =="
+SUBNET_IDS=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$VPC_ID" --query "Subnets[*].SubnetId" --output text)
+if [ -z "$SUBNET_IDS" ]; then
+    echo "No Subnets found."
+else
+    for SUBNET_ID in $SUBNET_IDS; do
+        echo "Deleting Subnet $SUBNET_ID"
+        aws ec2 delete-subnet --subnet-id $SUBNET_ID
+    done
+fi
+
+# Delete any route tables associated with the VPC
+echo "== Deleting route tables =="
+ROUTE_TABLE_IDS=$(aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$VPC_ID" --query "RouteTables[?Associations[0].Main!=true].RouteTableId" --output text)
+if [ -z "$ROUTE_TABLE_IDS" ]; then
+    echo "No Route Tables found."
+else
+    for ROUTE_TABLE_ID in $ROUTE_TABLE_IDS; do
+        echo "Disassociating Route Table Association $ASSOCIATION_ID"
+        aws ec2 disassociate-route-table --association-id $ASSOCIATION_ID
+    
+        echo "Deleting Route Table $ROUTE_TABLE_ID"
+        aws ec2 delete-route-table --route-table-id $ROUTE_TABLE_ID
+    done
+fi
 
 # Finally, delete the VPC
 echo "== Deleting the VPC =="
